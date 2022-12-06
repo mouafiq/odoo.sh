930e533c4d5af552d7c1876468abe35f3dfa1a1b
Marc Wimmer <marc@itewimmer.de>
2022-12-06 16:32:52 +0100

    Merged: https://github.com/OCA/credit-control

---
+++ OCA/credit-control/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-credit-control",
+    description="Meta package for oca-credit-control Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_credit_control>=15.0dev,<15.1dev',
+        'odoo-addon-account_financial_risk>=15.0dev,<15.1dev',
+        'odoo-addon-account_invoice_overdue_reminder>=15.0dev,<15.1dev',
+        'odoo-addon-partner_risk_insurance>=15.0dev,<15.1dev',
+        'odoo-addon-sale_financial_risk>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
