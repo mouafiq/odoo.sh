dc1a629ff786a2e93a0bbc9d1748c8aa34776482
Marc Wimmer <marc@itewimmer.de>
2022-12-06 09:53:11 +0100

    Merged: https://github.com/OCA/l10n-italy

---
+++ OCA/l10n-italy/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-l10n-italy",
+    description="Meta package for oca-l10n-italy Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-l10n_it_abicab>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_account>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_account_stamp>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_account_tax_kind>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_appointment_code>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_ateco>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_delivery_note_base>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_fiscal_document_type>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_fiscal_payment_term>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_fiscalcode>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_ipa>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_payment_reason>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_pec>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_rea>=16.0dev,<16.1dev',
+        'odoo-addon-l10n_it_vat_payability>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
