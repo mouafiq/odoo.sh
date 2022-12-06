370a304a70d841184fa407b081ef4a75e88d90b3
Marc Wimmer <marc@itewimmer.de>
2022-12-06 09:55:10 +0100

    Merged: https://github.com/OCA/multi-company

---
+++ OCA/multi-company/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-multi-company",
+    description="Meta package for oca-multi-company Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_invoice_inter_company>=15.0dev,<15.1dev',
+        'odoo-addon-base_multi_company>=15.0dev,<15.1dev',
+        'odoo-addon-partner_multi_company>=15.0dev,<15.1dev',
+        'odoo-addon-product_multi_company>=15.0dev,<15.1dev',
+        'odoo-addon-product_tax_multicompany_default>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
