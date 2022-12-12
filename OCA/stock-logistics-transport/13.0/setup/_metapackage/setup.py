64558aab0192b02eeaad3860d4db39a9050f82ca
Marc Wimmer <marc@itewimmer.de>
2022-12-12 17:49:38 +0100

    Merged: https://github.com/OCA/stock-logistics-transport

---
+++ OCA/stock-logistics-transport/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-stock-logistics-transport",
+    description="Meta package for oca-stock-logistics-transport Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-shipment_advice',
+        'odoo13-addon-stock_dock',
+        'odoo13-addon-stock_location_address',
+        'odoo13-addon-stock_location_address_purchase',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
