7b1245c194dd6d120c8818451820891bdcb36a9a
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:54:36 +0100

    Merged: https://github.com/OCA/interface-github

---
+++ OCA/interface-github/12.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-interface-github",
+    description="Meta package for oca-interface-github Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-github_connector',
+        'odoo12-addon-github_connector_oca',
+        'odoo12-addon-github_connector_odoo',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 12.0',
+    ]
+)
