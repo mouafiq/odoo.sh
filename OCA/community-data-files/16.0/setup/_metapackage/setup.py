1b9e6e5f0388cfe9fe46c11e9119f6d144fa3b94
Marc Wimmer <marc@itewimmer.de>
2022-12-12 10:00:57 +0100

    Merged: https://github.com/OCA/community-data-files

---
+++ OCA/community-data-files/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-community-data-files",
+    description="Meta package for oca-community-data-files Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_payment_unece>=16.0dev,<16.1dev',
+        'odoo-addon-account_tax_unece>=16.0dev,<16.1dev',
+        'odoo-addon-base_iso3166>=16.0dev,<16.1dev',
+        'odoo-addon-base_unece>=16.0dev,<16.1dev',
+        'odoo-addon-uom_unece>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
