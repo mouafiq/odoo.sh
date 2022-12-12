8f1e3884059f83da8ca9d8dd8d639433f647cd91
Marc Wimmer <marc@itewimmer.de>
2022-12-12 12:36:49 +0100

    Merged: https://github.com/OCA/account-invoice-reporting

---
+++ OCA/account-invoice-reporting/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-account-invoice-reporting",
+    description="Meta package for oca-account-invoice-reporting Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_comment_template>=15.0dev,<15.1dev',
+        'odoo-addon-account_invoice_line_sale_line_position>=15.0dev,<15.1dev',
+        'odoo-addon-account_invoice_report_due_list>=15.0dev,<15.1dev',
+        'odoo-addon-account_invoice_report_payment_info>=15.0dev,<15.1dev',
+        'odoo-addon-account_reporting_volume>=15.0dev,<15.1dev',
+        'odoo-addon-account_reporting_weight>=15.0dev,<15.1dev',
+        'odoo-addon-partner_time_to_pay>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
