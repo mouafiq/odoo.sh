07fd73c74fe525840d3499c0b1825e6b93f88668
Marc Wimmer <marc@itewimmer.de>
2022-12-12 14:32:43 +0100

    Merged: https://github.com/OCA/vertical-hotel

---
+++ OCA/vertical-hotel/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-vertical-hotel",
+    description="Meta package for oca-vertical-hotel Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-hotel',
+        'odoo13-addon-hotel_housekeeping',
+        'odoo13-addon-hotel_reservation',
+        'odoo13-addon-hotel_restaurant',
+        'odoo13-addon-report_hotel_reservation',
+        'odoo13-addon-report_hotel_restaurant',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
