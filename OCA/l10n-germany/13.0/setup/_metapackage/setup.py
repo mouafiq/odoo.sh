7e58e1288d7129e35dffd479b88c660c9bb737b0
Marc Wimmer <marc@itewimmer.de>
2022-12-12 17:48:24 +0100

    Merged: https://github.com/OCA/l10n-germany

---
+++ OCA/l10n-germany/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-l10n-germany",
+    description="Meta package for oca-l10n-germany Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-l10n_de_country_states',
+        'odoo13-addon-l10n_de_holidays',
+        'odoo13-addon-l10n_de_skr03_mis_reports',
+        'odoo13-addon-l10n_de_skr04_mis_reports',
+        'odoo13-addon-l10n_de_tax_statement',
+        'odoo13-addon-l10n_de_toponyms',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
