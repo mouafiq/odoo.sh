23a75b403419092f2d2039058fdc43bb256b0bc0
Marc Wimmer <marc@itewimmer.de>
2022-12-12 09:44:52 +0100

    Merged: https://github.com/OCA/payroll

---
+++ OCA/payroll/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-payroll",
+    description="Meta package for oca-payroll Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-payroll',
+        'odoo13-addon-payroll_account',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
