bda8b5c11f67b04d814ca1847f4542fdfa3894a0
Marc Wimmer <marc@itewimmer.de>
2022-12-12 09:56:33 +0100

    Merged: https://github.com/OCA/hr

---
+++ OCA/hr/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-hr",
+    description="Meta package for oca-hr Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-hr_employee_firstname>=16.0dev,<16.1dev',
+        'odoo-addon-hr_employee_lastnames>=16.0dev,<16.1dev',
+        'odoo-addon-hr_employee_partner_external>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
