1f61aa585cce0417c9fc9e23939388b0923088c2
Marc Wimmer <marc@itewimmer.de>
2022-12-12 12:05:36 +0100

    Merged: https://github.com/OCA/website

---
+++ OCA/website/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-website",
+    description="Meta package for oca-website Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-website_cookiefirst>=15.0dev,<15.1dev',
+        'odoo-addon-website_crm_privacy_policy>=15.0dev,<15.1dev',
+        'odoo-addon-website_crm_quick_answer>=15.0dev,<15.1dev',
+        'odoo-addon-website_google_tag_manager>=15.0dev,<15.1dev',
+        'odoo-addon-website_legal_page>=15.0dev,<15.1dev',
+        'odoo-addon-website_odoo_debranding>=15.0dev,<15.1dev',
+        'odoo-addon-website_snippet_country_dropdown>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
