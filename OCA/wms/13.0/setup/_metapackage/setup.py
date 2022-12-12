b4843fb293196b84908d268b5b6e16aa9be612f5
Marc Wimmer <marc@itewimmer.de>
2022-12-12 10:17:19 +0100

    Merged: https://github.com/OCA/wms

---
+++ OCA/wms/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-wms",
+    description="Meta package for oca-wms Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-delivery_carrier_preference',
+        'odoo13-addon-delivery_carrier_warehouse',
+        'odoo13-addon-delivery_preference_glue_stock_picking_group',
+        'odoo13-addon-sale_stock_available_to_promise_release',
+        'odoo13-addon-sale_stock_available_to_promise_release_cutoff',
+        'odoo13-addon-sale_stock_available_to_promise_release_dropshipping',
+        'odoo13-addon-shopfloor',
+        'odoo13-addon-shopfloor_base',
+        'odoo13-addon-shopfloor_batch_automatic_creation',
+        'odoo13-addon-shopfloor_checkout_sync',
+        'odoo13-addon-shopfloor_delivery_shipment',
+        'odoo13-addon-shopfloor_delivery_shipment_mobile',
+        'odoo13-addon-shopfloor_example',
+        'odoo13-addon-shopfloor_mobile',
+        'odoo13-addon-shopfloor_mobile_base',
+        'odoo13-addon-shopfloor_packing_info',
+        'odoo13-addon-shopfloor_rest_log',
+        'odoo13-addon-shopfloor_workstation',
+        'odoo13-addon-shopfloor_workstation_mobile',
+        'odoo13-addon-stock_available_to_promise_release',
+        'odoo13-addon-stock_available_to_promise_release_dynamic_routing',
+        'odoo13-addon-stock_checkout_sync',
+        'odoo13-addon-stock_dynamic_routing',
+        'odoo13-addon-stock_dynamic_routing_checkout_sync',
+        'odoo13-addon-stock_dynamic_routing_reserve_rule',
+        'odoo13-addon-stock_move_source_relocate',
+        'odoo13-addon-stock_move_source_relocate_dynamic_routing',
+        'odoo13-addon-stock_picking_type_shipping_policy',
+        'odoo13-addon-stock_picking_type_shipping_policy_group_by',
+        'odoo13-addon-stock_reception_screen',
+        'odoo13-addon-stock_reception_screen_measuring_device',
+        'odoo13-addon-stock_reception_screen_mrp_subcontracting',
+        'odoo13-addon-stock_reception_screen_qty_by_packaging',
+        'odoo13-addon-stock_storage_type',
+        'odoo13-addon-stock_storage_type_buffer',
+        'odoo13-addon-stock_storage_type_putaway_abc',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
