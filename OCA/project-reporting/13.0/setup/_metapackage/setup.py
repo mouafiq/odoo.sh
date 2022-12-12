5a34d1e04e8c2e25d604a432c1d322bda188928b
Marc Wimmer <marc@itewimmer.de>
2022-12-12 18:31:17 +0100

    Merged: https://github.com/OCA/project-reporting

---
+++ OCA/project-reporting/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-project-reporting",
+    description="Meta package for oca-project-reporting Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-project_task_report',
+        'odoo13-addon-project_task_timesheet_report',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
