961340b1d900ee155a99244dbc366c6c9e240217
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:24:43 +0100

    Merged: https://github.com/OCA/product-variant

---
+++ OCA/product-variant/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-product-variant",
+    description="Meta package for oca-product-variant Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-product_matrix_show_color',
+        'odoo14-addon-product_matrix_sticky_header',
+        'odoo14-addon-product_variant_configurator',
+        'odoo14-addon-product_variant_default_code',
+        'odoo14-addon-product_variant_inactive',
+        'odoo14-addon-product_variant_sale_price',
+        'odoo14-addon-sale_order_line_variant_description',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
