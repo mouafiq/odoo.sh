3b121b90ecbf37a3a29ae37e04b9be6fc10c2a0d
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:22:50 +0100

    Merged: https://github.com/OCA/account-closing

---
+++ OCA/account-closing/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-account-closing",
+    description="Meta package for oca-account-closing Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-account_cutoff_accrual_picking',
+        'odoo14-addon-account_cutoff_accrual_subscription',
+        'odoo14-addon-account_cutoff_base',
+        'odoo14-addon-account_cutoff_start_end_dates',
+        'odoo14-addon-account_fiscal_year_closing',
+        'odoo14-addon-account_invoice_start_end_dates',
+        'odoo14-addon-account_multicurrency_revaluation',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
