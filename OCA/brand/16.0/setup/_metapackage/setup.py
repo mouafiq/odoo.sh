dcee629cedac25db6c04b8870619705627a9c1a3
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:44:19 +0100

    Merged: https://github.com/OCA/brand

---
+++ OCA/brand/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-brand",
+    description="Meta package for oca-brand Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_brand>=16.0dev,<16.1dev',
+        'odoo-addon-brand>=16.0dev,<16.1dev',
+        'odoo-addon-brand_external_report_layout>=16.0dev,<16.1dev',
+        'odoo-addon-product_brand>=16.0dev,<16.1dev',
+        'odoo-addon-sale_brand>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
