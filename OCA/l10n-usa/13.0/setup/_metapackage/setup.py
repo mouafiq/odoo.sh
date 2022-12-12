7365ecfc907f4aef7b3418fbf0eb8745cb45515e
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:56:09 +0100

    Merged: https://github.com/OCA/l10n-usa

---
+++ OCA/l10n-usa/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-l10n-usa",
+    description="Meta package for oca-l10n-usa Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-l10n_us_form_1099',
+        'odoo13-addon-l10n_us_gaap',
+        'odoo13-addon-l10n_us_gaap_mis_report',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
