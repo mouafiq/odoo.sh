e8ac485b52ca61f3a460ca1d40ddb2f3b4a27161
Marc Wimmer <marc@itewimmer.de>
2022-12-12 13:49:01 +0100

    Merged: https://github.com/OCA/queue

---
+++ OCA/queue/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-queue",
+    description="Meta package for oca-queue Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-queue_job>=16.0dev,<16.1dev',
+        'odoo-addon-queue_job_cron>=16.0dev,<16.1dev',
+        'odoo-addon-test_queue_job>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
