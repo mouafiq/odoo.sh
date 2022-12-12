6049cb3f6f17e741402ba42d5a583c49e7e6c6a7
Marc Wimmer <marc@itewimmer.de>
2022-12-12 15:45:37 +0100

    Merged: https://github.com/OCA/server-backend

---
+++ OCA/server-backend/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-server-backend",
+    description="Meta package for oca-server-backend Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-base_external_dbsource>=15.0dev,<15.1dev',
+        'odoo-addon-base_external_dbsource_mssql>=15.0dev,<15.1dev',
+        'odoo-addon-base_external_dbsource_sqlite>=15.0dev,<15.1dev',
+        'odoo-addon-base_external_system>=15.0dev,<15.1dev',
+        'odoo-addon-base_global_discount>=15.0dev,<15.1dev',
+        'odoo-addon-base_import_match>=15.0dev,<15.1dev',
+        'odoo-addon-base_user_role>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
