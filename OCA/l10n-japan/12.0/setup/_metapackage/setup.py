11ec2edde4d344436ca272bf9d9b5b122c4c8662
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:23:05 +0100

    Merged: https://github.com/OCA/l10n-japan

---
+++ OCA/l10n-japan/12.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-l10n-japan",
+    description="Meta package for oca-l10n-japan Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-l10n_jp_address_layout',
+        'odoo12-addon-l10n_jp_country_state',
+        'odoo12-addon-l10n_jp_partner_title_qweb',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 12.0',
+    ]
+)
