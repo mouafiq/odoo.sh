834acaef31ce4960b5c95d00540adced1cc0a7f4
Marc Wimmer <marc@itewimmer.de>
2022-12-12 12:10:24 +0100

    Merged: https://github.com/OCA/intrastat-extrastat

---
+++ OCA/intrastat-extrastat/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-intrastat-extrastat",
+    description="Meta package for oca-intrastat-extrastat Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-intrastat_base>=15.0dev,<15.1dev',
+        'odoo-addon-intrastat_product>=15.0dev,<15.1dev',
+        'odoo-addon-product_harmonized_system>=15.0dev,<15.1dev',
+        'odoo-addon-product_harmonized_system_delivery>=15.0dev,<15.1dev',
+        'odoo-addon-product_harmonized_system_stock>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
