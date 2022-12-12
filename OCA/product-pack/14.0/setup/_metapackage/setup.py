285646ae92ad67b5f08bebe345f77a074ed20763
Marc Wimmer <marc@itewimmer.de>
2022-12-12 14:07:17 +0100

    Merged: https://github.com/OCA/product-pack

---
+++ OCA/product-pack/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-product-pack",
+    description="Meta package for oca-product-pack Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-product_pack',
+        'odoo14-addon-sale_product_pack',
+        'odoo14-addon-stock_product_pack',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
