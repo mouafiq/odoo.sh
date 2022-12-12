67d527002a42bc585d5bb77efb4eba55fc08cf75
Marc Wimmer <marc@itewimmer.de>
2022-12-12 20:55:11 +0100

    Merged: https://github.com/OCA/l10n-mexico

---
+++ OCA/l10n-mexico/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-l10n-mexico",
+    description="Meta package for oca-l10n-mexico Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-l10n_mx_res_partner',
+        'odoo13-addon-l10n_mx_sat_account',
+        'odoo13-addon-l10n_mx_sat_reference',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
