4b8955012ce0d8319a1218da1a4103d5203752a3
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:26:42 +0100

    Merged: https://github.com/OCA/account-invoicing

---
+++ OCA/account-invoicing/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-account-invoicing",
+    description="Meta package for oca-account-invoicing Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_invoice_blocking>=16.0dev,<16.1dev',
+        'odoo-addon-account_invoice_fiscal_position_update>=16.0dev,<16.1dev',
+        'odoo-addon-account_invoice_tax_required>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
