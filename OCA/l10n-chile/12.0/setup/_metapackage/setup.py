a80fd2b664c334838184902ee6e7814141745c86
Marc Wimmer <marc@itewimmer.de>
2022-12-12 21:12:08 +0100

    Merged: https://github.com/OCA/l10n-chile

---
+++ OCA/l10n-chile/12.0/setup/_metapackage/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-l10n-chile",
+    description="Meta package for oca-l10n-chile Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-connector_acp',
+        'odoo12-addon-connector_acp_ftp',
+        'odoo12-addon-connector_dicom',
+        'odoo12-addon-connector_dicom_crm',
+        'odoo12-addon-l10n_cl_chart_of_account',
+        'odoo12-addon-l10n_cl_currency_rate_sbif',
+        'odoo12-addon-l10n_cl_etd',
+        'odoo12-addon-l10n_cl_etd_account',
+        'odoo12-addon-l10n_cl_etd_stock',
+        'odoo12-addon-l10n_cl_etd_xerox',
+        'odoo12-addon-l10n_cl_etd_xerox_fsmroute',
+        'odoo12-addon-l10n_cl_invoicing_policy',
+        'odoo12-addon-l10n_cl_product',
+        'odoo12-addon-l10n_cl_sii',
+        'odoo12-addon-l10n_cl_sii_activity',
+        'odoo12-addon-l10n_cl_sii_folio',
+        'odoo12-addon-l10n_cl_sii_reference',
+        'odoo12-addon-l10n_cl_sii_reference_account',
+        'odoo12-addon-l10n_cl_toponym',
+        'odoo12-addon-res_partner_email_etd',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 12.0',
+    ]
+)
