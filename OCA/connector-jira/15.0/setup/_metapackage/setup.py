a4928c8bf9b4bafe6d6ea3400ba93f625a60200c
Marc Wimmer <marc@itewimmer.de>
2022-12-12 12:04:28 +0100

    Merged: https://github.com/OCA/connector-jira

---
+++ OCA/connector-jira/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-connector-jira",
+    description="Meta package for oca-connector-jira Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-connector_jira>=15.0dev,<15.1dev',
+        'odoo-addon-connector_jira_servicedesk>=15.0dev,<15.1dev',
+        'odoo-addon-connector_jira_tempo>=15.0dev,<15.1dev',
+        'odoo-addon-connector_jira_tempo_base>=15.0dev,<15.1dev',
+        'odoo-addon-server_env_connector_jira>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
