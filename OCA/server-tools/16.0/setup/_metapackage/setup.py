b9f3f251ddc14cd8dbe59a2a04234c06dd51bf30
Marc Wimmer <marc@itewimmer.de>
2022-12-12 09:54:53 +0100

    Merged: https://github.com/OCA/server-tools

---
+++ OCA/server-tools/16.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-server-tools",
+    description="Meta package for oca-server-tools Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-base_fontawesome>=16.0dev,<16.1dev',
+        'odoo-addon-base_m2m_custom_field>=16.0dev,<16.1dev',
+        'odoo-addon-base_view_inheritance_extension>=16.0dev,<16.1dev',
+        'odoo-addon-module_change_auto_install>=16.0dev,<16.1dev',
+        'odoo-addon-upgrade_analysis>=16.0dev,<16.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 16.0',
+    ]
+)
