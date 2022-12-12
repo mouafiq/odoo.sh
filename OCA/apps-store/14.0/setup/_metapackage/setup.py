e997851e81f5a802157492a8f0ff122920756da5
Marc Wimmer <marc@itewimmer.de>
2022-12-12 21:34:47 +0100

    Merged: https://github.com/OCA/apps-store

---
+++ OCA/apps-store/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-apps-store",
+    description="Meta package for oca-apps-store Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-apps_download',
+        'odoo14-addon-apps_product_creator',
+        'odoo14-addon-website_apps_store',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
