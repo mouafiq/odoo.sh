a00968b3a37c36891d938328a0ed2893784136b2
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:57:23 +0100

    Merged: https://github.com/OCA/donation

---
+++ OCA/donation/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-donation",
+    description="Meta package for oca-donation Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-donation',
+        'odoo13-addon-donation_base',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
