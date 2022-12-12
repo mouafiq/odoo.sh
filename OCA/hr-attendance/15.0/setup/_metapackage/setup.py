6cec61966e8729ded9dea77dd92cb717e95dcea3
Marc Wimmer <marc@itewimmer.de>
2022-12-12 12:00:06 +0100

    Merged: https://github.com/OCA/hr-attendance

---
+++ OCA/hr-attendance/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-hr-attendance",
+    description="Meta package for oca-hr-attendance Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-hr_attendance_autoclose>=15.0dev,<15.1dev',
+        'odoo-addon-hr_attendance_geolocation>=15.0dev,<15.1dev',
+        'odoo-addon-hr_attendance_reason>=15.0dev,<15.1dev',
+        'odoo-addon-hr_attendance_report_theoretical_time>=15.0dev,<15.1dev',
+        'odoo-addon-hr_attendance_rfid>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
