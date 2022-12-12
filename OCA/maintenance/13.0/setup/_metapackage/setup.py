bc4a08daf8177d0aa9607178f4e16ce0fecda561
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:36:19 +0100

    Merged: https://github.com/OCA/maintenance

---
+++ OCA/maintenance/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-maintenance",
+    description="Meta package for oca-maintenance Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-base_maintenance',
+        'odoo13-addon-base_maintenance_config',
+        'odoo13-addon-base_maintenance_group',
+        'odoo13-addon-maintenance_equipment_always_fold',
+        'odoo13-addon-maintenance_equipment_category_kanban',
+        'odoo13-addon-maintenance_equipment_contract',
+        'odoo13-addon-maintenance_equipment_custom_info',
+        'odoo13-addon-maintenance_equipment_hierarchy',
+        'odoo13-addon-maintenance_equipment_scrap',
+        'odoo13-addon-maintenance_equipment_sequence',
+        'odoo13-addon-maintenance_equipment_status',
+        'odoo13-addon-maintenance_equipment_tags',
+        'odoo13-addon-maintenance_plan',
+        'odoo13-addon-maintenance_plan_activity',
+        'odoo13-addon-maintenance_project',
+        'odoo13-addon-maintenance_project_plan',
+        'odoo13-addon-maintenance_remote',
+        'odoo13-addon-maintenance_request_sequence',
+        'odoo13-addon-maintenance_request_stage_transition',
+        'odoo13-addon-maintenance_team_hierarchy',
+        'odoo13-addon-maintenance_timesheet',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
