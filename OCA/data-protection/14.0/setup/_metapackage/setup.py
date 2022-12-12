1d52af548e161c35364df8086336c1c721e84f5b
Marc Wimmer <marc@itewimmer.de>
2022-12-12 15:46:35 +0100

    Merged: https://github.com/OCA/data-protection

---
+++ OCA/data-protection/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-data-protection",
+    description="Meta package for oca-data-protection Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-privacy',
+        'odoo14-addon-privacy_consent',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
