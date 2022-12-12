fa6d5d9b6efde9d7a4ae01074adf570f8c03dd4a
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:40:02 +0100

    Merged: https://github.com/OCA/account-financial-reporting

---
+++ OCA/account-financial-reporting/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-account-financial-reporting",
+    description="Meta package for oca-account-financial-reporting Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-account_financial_report>=15.0dev,<15.1dev',
+        'odoo-addon-account_tax_balance>=15.0dev,<15.1dev',
+        'odoo-addon-mis_builder_cash_flow>=15.0dev,<15.1dev',
+        'odoo-addon-partner_statement>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
