485c8138ac363a2c36d9f2c55476a41753672a7a
Marc Wimmer <marc@itewimmer.de>
2022-12-12 11:38:58 +0100

    Merged: https://github.com/OCA/e-commerce

---
+++ OCA/e-commerce/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-e-commerce",
+    description="Meta package for oca-e-commerce Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-website_sale_cart_expire>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_checkout_country_vat>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_checkout_skip_payment>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_comparison_hide_price>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_hide_empty_category>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_hide_price>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_product_brand>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_product_description>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_product_detail_attribute_value_image>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_product_reference_displayed>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_require_login>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_secondary_unit>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_suggest_create_account>=15.0dev,<15.1dev',
+        'odoo-addon-website_sale_wishlist_keep>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
