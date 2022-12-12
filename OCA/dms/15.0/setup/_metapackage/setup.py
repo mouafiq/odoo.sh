24cc17fd57167235c7b103ffee85a8e54524d0d7
Marc Wimmer <marc@itewimmer.de>
2022-12-12 16:43:00 +0100

    Merged: https://github.com/OCA/dms

---
+++ OCA/dms/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-dms",
+    description="Meta package for oca-dms Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-dms>=15.0dev,<15.1dev',
+        'odoo-addon-dms_field>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
