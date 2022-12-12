c3db85e82ea68554f05eb4c895aa026e4b7743fa
Marc Wimmer <marc@itewimmer.de>
2022-12-12 09:58:54 +0100

    Merged: https://github.com/OCA/rma

---
+++ OCA/rma/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-rma",
+    description="Meta package for oca-rma Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-product_warranty>=15.0dev,<15.1dev',
+        'odoo-addon-rma>=15.0dev,<15.1dev',
+        'odoo-addon-rma_sale>=15.0dev,<15.1dev',
+        'odoo-addon-rma_sale_mrp>=15.0dev,<15.1dev',
+        'odoo-addon-stock_production_lot_warranty>=15.0dev,<15.1dev',
+        'odoo-addon-website_rma>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
