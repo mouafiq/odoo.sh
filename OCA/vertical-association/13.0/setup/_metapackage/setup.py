5a3938c6787029c0725ce700161eee04d629f4dc
Marc Wimmer <marc@itewimmer.de>
2022-12-12 17:57:13 +0100

    Merged: https://github.com/OCA/vertical-association

---
+++ OCA/vertical-association/13.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-vertical-association",
+    description="Meta package for oca-vertical-association Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-contract_membership_delegated_partner',
+        'odoo13-addon-membership_delegated_partner',
+        'odoo13-addon-membership_extension',
+        'odoo13-addon-membership_initial_fee',
+        'odoo13-addon-membership_variable_period',
+        'odoo13-addon-membership_withdrawal',
+        'odoo13-addon-website_membership_random_order',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 13.0',
+    ]
+)
