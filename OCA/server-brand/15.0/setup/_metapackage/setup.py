ec6f1502bf27d97a299687a4ffe5f9f43529f1dd
Marc Wimmer <marc@itewimmer.de>
2022-12-12 18:26:38 +0100

    Merged: https://github.com/OCA/server-brand

---
+++ OCA/server-brand/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-server-brand",
+    description="Meta package for oca-server-brand Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-disable_odoo_online>=15.0dev,<15.1dev',
+        'odoo-addon-portal_odoo_debranding>=15.0dev,<15.1dev',
+        'odoo-addon-remove_odoo_enterprise>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
