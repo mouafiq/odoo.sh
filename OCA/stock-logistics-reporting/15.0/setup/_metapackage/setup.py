01f416a56508c892512a6a3b30d8c53440769b96
Marc Wimmer <marc@itewimmer.de>
2022-12-12 10:02:36 +0100

    Merged: https://github.com/OCA/stock-logistics-reporting

---
+++ OCA/stock-logistics-reporting/15.0/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-stock-logistics-reporting",
+    description="Meta package for oca-stock-logistics-reporting Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-delivery_line_sale_line_position>=15.0dev,<15.1dev',
+        'odoo-addon-stock_account_valuation_report>=15.0dev,<15.1dev',
+        'odoo-addon-stock_card_report>=15.0dev,<15.1dev',
+        'odoo-addon-stock_move_value_report>=15.0dev,<15.1dev',
+        'odoo-addon-stock_picking_report_undelivered_product>=15.0dev,<15.1dev',
+        'odoo-addon-stock_picking_report_valued>=15.0dev,<15.1dev',
+        'odoo-addon-stock_report_quantity_by_location>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
