dbc904ef0c836e1f86151c4e4ba4779dffd5b0ae
Marc Wimmer <marc@itewimmer.de>
2022-12-12 14:31:42 +0100

    Merged: https://github.com/OCA/margin-analysis

---
+++ OCA/margin-analysis/14.0/setup/_metapackage/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-margin-analysis",
+    description="Meta package for oca-margin-analysis Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-account_invoice_margin',
+        'odoo14-addon-account_invoice_margin_sale',
+        'odoo14-addon-sale_margin_delivery_cost',
+        'odoo14-addon-sale_margin_security',
+        'odoo14-addon-sale_margin_sync',
+        'odoo14-addon-sale_report_margin',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
